f8c2af9832e91d7b6430e7d9d058de29c8ad9d4e
Yanis Lalou <ylalou.g@gmail.com>
2024-03-08 17:01:00 +0100

    Add TRANSFER_COMPONENT_ANALYSIS solver

---
+++ solvers/TRANSFER_COMPONENT_ANALYSIS.py
@@ -0,0 +1,34 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from skada import TransferComponentAnalysisAdapter, make_da_pipeline
+    from benchmark_utils.base_solver import DASolver
+    from xgboost import XGBClassifier
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DASolver):
+
+    # Name to select the solver in the CLI and to display the results.
+    name = 'TRANSFER_COMPONENT_ANALYSIS'
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    param_grid = {
+        'transfercomponentanalysisadapter__kernel': ['rbf'],
+        'transfercomponentanalysisadapter__n_components': [2, 3],
+        'transfercomponentanalysisadapter__mu': [0.01, 0.1]
+    }
+
+    def get_estimator(self):
+        # return CORAL()
+        # The estimator passed should have a 'predict_proba' method.
+        return make_da_pipeline(
+            TransferComponentAnalysisAdapter(),
+            XGBClassifier()
+        )
