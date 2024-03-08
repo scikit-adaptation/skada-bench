81f0dcaf6801289c52bc394637a09236f6dd4e63
Yanis Lalou <ylalou.g@gmail.com>
2024-03-08 16:51:49 +0100

    Add SUBSPACE_ALIGNMENT solver

---
+++ solvers/SUBSPACE_ALIGNMENT.py
@@ -0,0 +1,32 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from skada import SubspaceAlignmentAdapter, make_da_pipeline
+    from benchmark_utils.base_solver import DASolver
+    from xgboost import XGBClassifier
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DASolver):
+
+    # Name to select the solver in the CLI and to display the results.
+    name = 'SUBSPACE_ALIGNMENT'
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    param_grid = {
+        'subspacealignmentadapter__n_components': [2, 4, 8, 16],
+    }
+
+    def get_estimator(self):
+        # return CORAL()
+        # The estimator passed should have a 'predict_proba' method.
+        return make_da_pipeline(
+            SubspaceAlignmentAdapter(),
+            XGBClassifier()
+        )
