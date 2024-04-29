83b72399d406cbdf50c3e3d3795d583b767d9afd
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2024-04-29 14:53:32 +0200

    add tjm

---
+++ solvers/TRANSFER_JOINT_MATCHING.py
@@ -0,0 +1,40 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from skada import TransferJointMatchingAdapter, make_da_pipeline
+    from benchmark_utils.base_solver import DASolver
+    from xgboost import XGBClassifier
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DASolver):
+
+    # Name to select the solver in the CLI and to display the results.
+    name = 'transfer_joint_matching'
+
+    requirements = [
+        "pip:xgboost",
+    ]
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    param_grid = {
+        'transferjointmatchingadapter__kernel': ['rbf'],
+        'transferjointmatchingadapter__n_components': [1, 2, 3],
+        'transferjointmatchingadapter__tradeoff': [0, 1e-4, 1e-2]
+    }
+
+    def get_estimator(self):
+        # return CORAL()
+        # The estimator passed should have a 'predict_proba' method.
+        return make_da_pipeline(
+            TransferJointMatchingAdapter(),
+            XGBClassifier()
+            .set_fit_request(sample_weight=True)
+            .set_score_request(sample_weight=True),
+        )
