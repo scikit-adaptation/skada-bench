2a7cf2de532e3671b8cf365002bea6a4ea758f16
Yanis Lalou <ylalou.g@gmail.com>
2024-05-03 16:27:42 +0200

    Add transfer_subspace_learning to the bench

---
+++ solvers/transfer_subspace_learning.py
@@ -0,0 +1,36 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from skada import TransferSubspaceLearningAdapter, make_da_pipeline
+    from benchmark_utils.base_solver import DASolver, FinalEstimator
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DASolver):
+    # Name to select the solver in the CLI and to display the results.
+    name = 'transfer_subspace_learning'
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    param_grid = {
+        'transfersubspacelearningadapter__n_components': [1, 2, 3],
+        'transfersubspacelearningadapter__base_method': ['pca', 'flda', 'lpp'],
+        'transfersubspacelearningadapter__length_scale': [1, 5, 10],
+        'transfersubspacelearningadapter__mu': [0.1, 1, 10],
+        'transfersubspacelearningadapter__reg': [1e-3, 0.1, 1,],
+        'transfersubspacelearningadapter__max_iter': [300],
+        'transfersubspacelearningadapter__tol': [1e-4],
+        'finalestimator__estimator_name': ["LR", "SVC", "XGB"],
+    }
+
+    def get_estimator(self):
+        # The estimator passed should have a 'predict_proba' method.
+        return make_da_pipeline(
+            TransferSubspaceLearningAdapter(),
+            FinalEstimator(),
+        )
