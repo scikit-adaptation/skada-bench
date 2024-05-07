e7d9cb91ae1d14d087539dca7b9758a3da380463
Rémi Flamary <remi.flamary@gmail.com>
2024-05-07 14:59:20 +0200

    add label propagation

---
+++ solvers/OTLabelProp.py
@@ -0,0 +1,31 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from skada import OTLabelPropAdapter, make_da_pipeline
+    from benchmark_utils.base_solver import DASolver, FinalEstimator
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DASolver):
+
+    # Name to select the solver in the CLI and to display the results.
+    name = 'OTLabelProp'
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    param_grid = {
+        'otlabelpropadapter__metric': ['sqeuclidean','euclidean'],
+        'otlabelpropadapter__reg': [None,],
+        'finalestimator__estimator_name': ["SVC", "XGB"],
+    }
+
+    def get_estimator(self):
+        # The estimator passed should have a 'predict_proba' method.
+        return make_da_pipeline(
+            OTLabelPropAdapter(),
+            FinalEstimator(),
+        )
