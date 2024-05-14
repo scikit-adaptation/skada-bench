f5fa082cb167108609f5b2bc6a2d76384be72968
Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>
2024-05-14 17:06:22 +0200

    Add DensityReweight + NearestNeighborReweight (#49)
    * Remove not useful param in amazonreview.py
    
    * Add Density reweight solver
    
    * typo fix
    
    * Add NNreweight
    
    * Add NN RW + Dens. RW to Reweigth tables
    
    * Update param grid for NN RW

---
+++ solvers/density_reweight.py
@@ -0,0 +1,27 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from skada import DensityReweightAdapter, make_da_pipeline
+    from benchmark_utils.base_solver import DASolver, FinalEstimator
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DASolver):
+    # Name to select the solver in the CLI and to display the results.
+    name = 'density_reweight'
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    param_grid = {'finalestimator__estimator_name': ["LR", "SVC", "XGB"],}
+
+    def get_estimator(self):
+        # The estimator passed should have a 'predict_proba' method.
+        return make_da_pipeline(
+            DensityReweightAdapter(),
+            FinalEstimator(),
+        )
