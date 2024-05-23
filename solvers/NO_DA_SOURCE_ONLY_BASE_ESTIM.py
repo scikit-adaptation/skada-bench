b004a855459074e6d8f4a1b012bb8934ec3b03c7
antoinedemathelin <45264923+antoinedemathelin@users.noreply.github.com>
2024-05-23 10:24:42 +0200

    Add config for finding best base estim (#71)

---
+++ solvers/NO_DA_SOURCE_ONLY_BASE_ESTIM.py
@@ -0,0 +1,29 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from benchmark_utils.base_solver import DASolver, FinalEstimator
+    from skada.base import SelectSource
+    from skada import make_da_pipeline
+    from skada.metrics import SupervisedScorer
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DASolver):
+    # Name to select the solver in the CLI and to display the results.
+    name = 'NO_DA_SOURCE_ONLY_BASE_ESTIM'
+
+    param_grid = {'finalestimator__estimator_name': ["LR", "SVC", "SVC_mnist_usps", "XGB"]}
+
+    def get_estimator(self):
+        # We only use the supervised scorer!
+        self.criterions = {
+            'supervised': SupervisedScorer()
+        }
+        # The estimator passed should have a 'predict_proba' method.
+        return make_da_pipeline(
+            ('finalestimator', SelectSource(FinalEstimator())),
+        )
