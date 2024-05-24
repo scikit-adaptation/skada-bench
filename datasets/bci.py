768fd6513445bde4aaa5383c9c77e30c38871757
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2024-05-24 14:42:05 +0200

    fix mne version

--- datasets/bci.py
+++ datasets/bci.py
@@ -29,7 +29,7 @@ class Dataset(BaseDataset):
 
     install_cmd = 'conda'
 
-    requirements = ['braindecode', 'moabb', 'pyriemann']
+    requirements = ['mne==1.6.1', 'braindecode', 'moabb', 'pyriemann']
 
     parameters = {
         'subject_id': [1, 2, 3, 4, 5, 6, 7, 8, 9],
