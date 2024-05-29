df7fdbf93dc2e1ff9769d55f0b3914a857e6c2f5
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2024-05-29 15:59:35 +0200

    subsample data

--- datasets/amazonreview.py
+++ datasets/amazonreview.py
@@ -1,6 +1,7 @@
 from benchopt import BaseDataset, safe_import_context
 
 with safe_import_context() as import_ctx:
+    import numpy as np
     import urllib.request
     from pathlib import Path
     import pickle
@@ -17,6 +18,7 @@ class Dataset(BaseDataset):
     # the cross product for each key in the dictionary.
     # Any parameters 'param' defined here is available as `self.param`.
     parameters = {
+        "n_samples_source, n_samples_target": [(2000, 2000)],
         "source_target": [
             ("books", "dvd"),
             ("books", "electronics"),
@@ -33,16 +35,15 @@ class Dataset(BaseDataset):
         ],
         "preprocessing": [
             "sentence_transformers"
-        ]
+        ],
+        "random_state": [27],
     }
 
     path_preprocessed_data = "data/amazon_review_preprocessed.pkl"
     url_preprocessed_data = "https://figshare.com/ndownloader/files/46642522?private_link=5b1a88c729389a8dbcd0"
 
-    def get_data(self):
-        # The return arguments of this function are passed as keyword arguments
-        # to `Objective.set_data`. This defines the benchmark's
-        # API to pass data. It is customizable for each benchmark.
+    def _get_dataset(self, dataset_name, n_samples=None):
+        rng = np.random.RandomState(self.random_state)
 
         # Check if the preprocessed data is available
         # If not, download them
@@ -55,15 +56,27 @@ class Dataset(BaseDataset):
 
         # Load preprocessed data
         with open(path_preprocessed_data, "rb") as f:
-            data_preprocessed = pickle.load(f)
+            data = pickle.load(f)
 
-        # Get source and target domains
-        source = self.source_target[0]
-        target = self.source_target[1]
-        X_source = data_preprocessed[source][self.preprocessing]
-        X_target = data_preprocessed[target][self.preprocessing]
-        y_source = data_preprocessed[source]['y']
-        y_target = data_preprocessed[target]['y']
+        # Sample data
+        X = data[dataset_name][self.preprocessing]
+        y = data[dataset_name]["y"]
+        if (n_samples is not None) and (n_samples < len(X)):
+            indices = rng.choice(len(X), n_samples, replace=False)
+            X, y = X[indices], y[indices]
+
+        return X, y
+
+    def get_data(self):
+        # The return arguments of this function are passed as keyword arguments
+        # to `Objective.set_data`. This defines the benchmark's
+        # API to pass data. It is customizable for each benchmark.
+
+        source, target = self.source_target
+        n_samples_source = self.n_samples_source
+        n_samples_target = self.n_samples_target
+        X_source, y_source = self._get_dataset(source, n_samples_source)
+        X_target, y_target = self._get_dataset(target, n_samples_target)
 
         # Merge source and target data
         X, y, sample_domain = source_target_merge(
