744e670270ca205289ce8c9a84ae11630f62c05d
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2024-05-29 14:05:25 +0200

    rm preprocessing + clean code

--- benchmark_utils/preprocess_amazonreview.py
+++ benchmark_utils/preprocess_amazonreview.py
@@ -5,42 +5,62 @@
 - The pickle file is stored in the datasets folder.
 """
 
-import urllib
+import urllib.request
 import gzip
 import tarfile
 import shutil
-import re
 from pathlib import Path
-
 import numpy as np
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
 
 
-def load_amazon(source, target, path=None):
-    """
-    Load sentiment analysis dataset giving source and target domain names
-    """
-    if path is None:
-        path = Path(__file__).parent
-    try:
-        file = open(path / "kitchen.txt")
-    except:
-        print("Downloading sentiment analysis data files...")
-        download_amazon(path)
-        print(
-            "Sentiment analysis data files successfully downloaded and saved in 'dataset/sa' folder"
+def download_amazon(path):
+    path = Path(path)
+    path.mkdir(parents=True, exist_ok=True)
+
+    # Check if the file is already downloaded
+    # If not, download it
+    if not (path / "kitchen.txt").exists():
+        urllib.request.urlretrieve(
+            "https://www.cs.jhu.edu/~mdredze/datasets/sentiment/domain_sentiment_data.tar.gz",
+            path / "domain_sentiment_data.tar.gz",
         )
-    return _get_Xy(source, target, path)
+        urllib.request.urlretrieve(
+            "https://www.cs.jhu.edu/~mdredze/datasets/sentiment/book.unlabeled.gz",
+            path / "book.unlabeled.gz",
+        )
+        tar = tarfile.open(path / "domain_sentiment_data.tar.gz", "r:gz")
+        tar.extractall(path / "domain_sentiment_data")
+        tar.close()
+        with gzip.open(path / "book.unlabeled.gz", "rb") as f_in:
+            with open(path / "books.txt", "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+        shutil.move(
+            path / "domain_sentiment_data/sorted_data_acl/dvd/unlabeled.review",
+            path / "dvd.txt",
+        )
+        shutil.move(
+            path / "domain_sentiment_data/sorted_data_acl/electronics/unlabeled.review",
+            path / "electronics.txt",
+        )
+        shutil.move(
+            path
+            / "domain_sentiment_data/sorted_data_acl/kitchen_&_housewares/unlabeled.review",
+            path / "kitchen.txt",
+        )
+
+        (path / "book.unlabeled.gz").unlink()
+        (path / "domain_sentiment_data.tar.gz").unlink()
+        shutil.rmtree(path / "domain_sentiment_data")
 
 
-def _get_reviews_and_labels_from_txt(file):
+def get_reviews(domain, path):
     """
     Open text file of amazon reviews and add all reviews and
     ratings in two list.
     max_ gives the maximum number of reviews to extract.
     """
-    file = open(file)
+    file = open(path / f"{domain}.txt")
 
     reviews = []
     labels = []
@@ -50,7 +70,7 @@ def _get_reviews_and_labels_from_txt(file):
         if capture_label:
             labels.append(float(line))
         if capture_review:
-            reviews.append(str(line))
+            reviews.append(str(line).strip())
 
         capture_label = False
         capture_review = False
@@ -62,109 +82,19 @@ def _get_reviews_and_labels_from_txt(file):
     return reviews, labels
 
 
-def _decontracted(phrase):
-    """
-    Decontract english common contraction
-    """
-    phrase = re.sub(r"won't", "will not", phrase)
-    phrase = re.sub(r"can't", "can not", phrase)
-    phrase = re.sub(r"n\'t", "not", phrase)
-    phrase = re.sub(r"\'re", "are", phrase)
-    phrase = re.sub(r"\'s", "is", phrase)
-    phrase = re.sub(r"\'d", "would", phrase)
-    phrase = re.sub(r"\'ll", "will", phrase)
-    phrase = re.sub(r"\'t", "not", phrase)
-    phrase = re.sub(r"\'ve", "have", phrase)
-    phrase = re.sub(r"\'m", "am", phrase)
-    return phrase
-
-
-def _preprocess_review(reviews):
-    """
-    Preprocess text in reviews list
-    """
-
-    # Download stopwords if not already downloaded
-    try:
-        stop = set(stopwords.words("english"))
-    except LookupError:
-        import nltk
-
-        nltk.download("stopwords")
-        stop = set(stopwords.words("english"))
-
-    snow = SnowballStemmer("english")
-
-    preprocessed_reviews = []
-
-    for sentence in reviews:
-        sentence = re.sub(r"http\S+", " ", sentence)
-        cleanr = re.compile("<.*?>")
-        sentence = re.sub(cleanr, " ", sentence)
-        sentence = _decontracted(sentence)
-        sentence = re.sub("\S\*\d\S*", " ", sentence)
-        sentence = re.sub("[^A-Za-z]+", " ", sentence)
-        sentence = re.sub(r'[?|!|\'|"|#]', r" ", sentence)
-        sentence = re.sub(r"[.|,|)|(|\|/]", r" ", sentence)
-        preprocessed_reviews.append(sentence.strip())
-
-    return preprocessed_reviews
-
-
-def get_reviews(domain, path):
-    """
-    Return preprocessed reviews and labels
-    """
-    reviews, labels = _get_reviews_and_labels_from_txt(path / f"{domain}.txt")
-    reviews = _preprocess_review(reviews)
-    return reviews, labels
-
-
-def download_amazon(path):
-    path = Path(path)
-    path.mkdir(parents=True, exist_ok=True)
-
-    urllib.request.urlretrieve(
-        "https://www.cs.jhu.edu/~mdredze/datasets/sentiment/domain_sentiment_data.tar.gz",
-        path / "domain_sentiment_data.tar.gz",
-    )
-    urllib.request.urlretrieve(
-        "https://www.cs.jhu.edu/~mdredze/datasets/sentiment/book.unlabeled.gz",
-        path / "book.unlabeled.gz",
-    )
-    tar = tarfile.open(path / "domain_sentiment_data.tar.gz", "r:gz")
-    tar.extractall(path / "domain_sentiment_data")
-    tar.close()
-    with gzip.open(path / "book.unlabeled.gz", "rb") as f_in:
-        with open(path / "books.txt", "wb") as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
-    shutil.move(
-        path / "domain_sentiment_data/sorted_data_acl/dvd/unlabeled.review",
-        path / "dvd.txt",
-    )
-    shutil.move(
-        path / "domain_sentiment_data/sorted_data_acl/electronics/unlabeled.review",
-        path / "electronics.txt",
-    )
-    shutil.move(
-        path / "domain_sentiment_data/sorted_data_acl/kitchen_&_housewares/unlabeled.review",
-        path / "kitchen.txt",
-    )
-
-    (path / "book.unlabeled.gz").unlink()
-    (path / "domain_sentiment_data.tar.gz").unlink()
-    shutil.rmtree(path / "domain_sentiment_data")
-
-
 if __name__ == "__main__":
-    PATH = Path("data/amazon_review")
+    PATH = Path("data")
     DOMAINS = ["books", "dvd", "electronics", "kitchen"]
 
     # Download data
-    download_amazon(PATH)
+    print("Downloading data...")
+    download_amazon(PATH / "amazon_review_raw")
 
     # Get (X, y) for each domain
+    print("Getting (X, y) for each domain...")
+    data_raw = dict()
     for domain in DOMAINS:
-        X, y = get_reviews(domain, PATH)
-
+        X, y = get_reviews(domain, PATH / "amazon_review_raw")
+        data_raw[domain] = {"X": X, "y": y}
+    for k, v in data_raw.items():
+        print(f"{k}: {len(v['X'])} reviews, {len(v['y'])} labels")
