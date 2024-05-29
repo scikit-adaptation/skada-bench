168e9d6a5bb9d2177791285dc772a74ff0b895a4
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2024-05-29 15:29:08 +0200

    load pickle in amazon_review dataset

--- datasets/amazonreview.py
+++ datasets/amazonreview.py
@@ -3,6 +3,8 @@ from benchopt import BaseDataset, safe_import_context
 with safe_import_context() as import_ctx:
     import scipy
     import numpy as np
+    from pathlib import Path
+    import pickle
     from sklearn.preprocessing import LabelEncoder
     from skada.utils import source_target_merge
     from skada.datasets import fetch_amazon_review_all
@@ -32,28 +34,41 @@ class Dataset(BaseDataset):
             ("kitchen", "dvd"),
             ("kitchen", "elec"),
         ],
+        "preprocessing": [
+            "sentence_transformers"
+        ]
     }
+
+    path_preprocessed_data = "data/amazon_review_preprocessed.pkl"
+    url_preprocessed_data = "https://figshare.com/ndownloader/files/XXXXX"
 
     def get_data(self):
         # The return arguments of this function are passed as keyword arguments
         # to `Objective.set_data`. This defines the benchmark's
         # API to pass data. It is customizable for each benchmark.
 
-        tmp_folder = "./data/amazon_review/"
-        dataset = fetch_amazon_review_all(data_home=tmp_folder)
+        # Check if the preprocessed data is available
+        # If not, download them
+        path_preprocessed_data = Path(self.path_preprocessed_data)
+        if not path_preprocessed_data.exists():
+            urllib.request.urlretrieve(
+                self.url_preprocessed_data,
+                path_preprocessed_data
+            )
 
+        # Load preprocessed data
+        with open(path_preprocessed_data, "rb") as f:
+            data_preprocessed = pickle.load(f)
+
+        # Get source and target domains
         source = self.source_target[0]
         target = self.source_target[1]
+        X_source = data_preprocessed[source][self.preprocessing]
+        X_target = data_preprocessed[target][self.preprocessing]
+        y_source = data_preprocessed[source]['y']
+        y_target = data_preprocessed[target]['y']
 
-        X_source, y_source = dataset.get_domain(source)
-        X_target, y_target = dataset.get_domain(target)
-
-        # XGBoost only supports labels in [0, num_classes-1]
-        le = LabelEncoder()
-        le.fit(np.concatenate([y_source, y_target]))
-        y_source = le.transform(y_source)
-        y_target = le.transform(y_target)
-
+        # Merge source and target data
         X, y, sample_domain = source_target_merge(
             X_source, X_target, y_source, y_target
         )
