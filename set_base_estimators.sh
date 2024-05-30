214cfd5aec2a9acd782deff1f3288a9504e0b6d1
antoinedemathelin <antoine.demat@gmail.com>
2024-05-30 12:57:03 +0200

    fix bash script

--- set_base_estimators.sh
+++ set_base_estimators.sh
@@ -1,5 +1,5 @@
 python benchmark_utils/generate_base_estim_config.py
 benchopt run --config config/find_best_base_estimators_per_dataset.yml
-python visualize/convert_benchopt_output_to_readable_csv.py
+python visualize/convert_benchopt_output_to_readable_csv.py --domain source --directory outputs --output results
 python benchmark_utils/extract_best_base_estim.py
 python benchmark_utils/generate_config_per_dataset.py
