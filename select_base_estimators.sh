26584524459851ab063dc740fecd5ad12a3ce931
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2024-05-31 14:01:28 +0200

    add --slurm option to select_base_estimators.sh

--- select_base_estimators.sh
+++ select_base_estimators.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+
+# Default SLURM config file
+SLURM_CONFIG=""
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --slurm) SLURM_CONFIG="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 # Set the config file for base estimator experiments for No DA Source Only
 python benchmark_utils/generate_base_estim_config.py
 
 # Run base estimator experiments for No DA Source Only. Store the results in `results_base_estimators/`
-benchopt run --config config/find_best_base_estimators_per_dataset.yml --no-plot --no-html
+if [ -n "$SLURM_CONFIG" ]; then
+    benchopt run --config config/find_best_base_estimators_per_dataset.yml --slurm $SLURM_CONFIG --no-plot --no-html
+else
+    benchopt run --config config/find_best_base_estimators_per_dataset.yml --no-plot --no-html
+fi
 
 # Clean the results and store them in `results_base_estimators/results_base_estim_experiments.csv`
 python visualize/convert_benchopt_output_to_readable_csv.py --domain source --directory outputs --output results_base_estimators --file_name results_base_estim_experiments
