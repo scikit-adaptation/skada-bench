bc75cdd87c406722958d6f7f6376d46401331b56
Théo Gnassounou <66993815+tgnassou@users.noreply.github.com>
2024-06-12 14:23:54 +0200

    [MRG] Add script to plot table and scatterplot (#83)
    * add script to compute the normalized data and create the table
    
    * update std
    
    * update
    
    * update
    
    * update
    
    * update scirpt
    
    * update
    
    * add plot for table per dateset
    
    * update plot files
    
    * update to works with given csv
    
    ---------
    
    Co-authored-by: Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>

---
+++ visualize/plot_results_per_dataset.py
@@ -0,0 +1,355 @@
+# %%
+import numpy as np
+import pandas as pd
+import json
+import argparse
+
+
+def shade_of_color(
+    df_value,
+    final_value,
+    min_value=0,
+    max_value=1,
+):
+    # Intensity range for the green and red colors
+    intensity_range = (30, 70)
+
+    if df_value == "nan" or np.isnan(df_value):
+        # Return the nan value
+        return final_value
+    elif df_value > min_value:
+        color_min = min_value
+        color_max = max_value
+        if color_max - color_min == 0:
+            # To avoid division by zero
+            intensity = intensity_range[1]
+        else:
+            intensity = int(
+                intensity_range[0]
+                + (intensity_range[1] - intensity_range[0])
+                * (df_value - color_min)
+                / (color_max - color_min)
+            )
+        return "\\cellcolor{green_color!%d}{%s}" % (intensity, final_value)
+    else:
+        return final_value
+
+
+def generate_table_results(
+    dataset,
+    csv_file,
+    csv_file_simulated,
+):
+    if dataset == "simulated":
+        df = pd.read_csv(csv_file_simulated)
+        print(df)
+    else:
+        df = pd.read_csv(csv_file)
+        df = df.query("dataset == @dataset")
+
+    df = df.query("estimator != 'NO_DA_SOURCE_ONLY_BASE_ESTIM'")
+
+    df["target_accuracy-test-identity"] = df["target_accuracy-test-identity"].apply(
+        lambda x: json.loads(x)
+    )
+
+    df["nb_splits"] = df["target_accuracy-test-identity"].apply(lambda x: len(x))
+
+    df_target = df.query('estimator == "Train Tgt" & scorer == "supervised"')
+    df_source = df.query(
+        'estimator == "Train Src" & scorer !=' '"supervised" & scorer != "best_scorer"'
+    )
+    idx_source_best_scorer = df_source.groupby(["shift"])[
+        "target_accuracy-test-mean"
+    ].idxmax()
+    df_source = df_source.loc[idx_source_best_scorer]
+
+    df = df.merge(
+        df_target[["shift", "target_accuracy-test-mean", "target_accuracy-test-std"]],
+        on="shift",
+        suffixes=("", "_target"),
+    )
+    df = df.merge(
+        df_source[
+            [
+                "shift",
+                "target_accuracy-test-mean",
+                "target_accuracy-test-std",
+                "target_accuracy-test-identity",
+            ]
+        ],
+        on="shift",
+        suffixes=("", "_source"),
+    )
+    # remove rows where the source is better than the target
+    df = df[
+        df["target_accuracy-test-mean_source"] < df["target_accuracy-test-mean_target"]
+    ].reset_index()
+    # check if nb_splits is 5 and 25 for the simulated dataset
+    df = df.query("nb_splits == 5 | nb_splits == 25")
+
+    # remove duplicates
+    df = df.drop_duplicates(subset=["dataset", "scorer", "estimator", "shift"])
+
+    df["rank"] = df.groupby(["dataset", "scorer", "shift"])[
+        "target_accuracy-test-mean"
+    ].rank(ascending=False)
+
+    df_best_scorer = pd.read_csv("./best_scorer.csv")
+    df_dataset = df.merge(
+        df_best_scorer[
+            [
+                "estimator",
+                "scorer",
+            ]
+        ],
+        on=[
+            "estimator",
+        ],
+        suffixes=("", "_best"),
+    )
+    df_dataset = df_dataset[
+        df_dataset["scorer"] == df_dataset["scorer_best"]
+    ].reset_index()
+    df_rank = df_dataset.groupby(["estimator"])["rank"].mean().reset_index()
+    # add mean \pm std column
+    df_dataset["acc_std"] = (
+        df_dataset["target_accuracy-test-mean"].round(2).astype(str)
+        + " $\pm$ "
+        + df_dataset["target_accuracy-test-std"].round(2).astype(str)
+    )
+
+    # create the table
+    df_tab = df_dataset.pivot(
+        index="shift", columns=["type", "estimator"], values="target_accuracy-test-mean"
+    )
+    df_tab_acc_std = df_dataset.pivot(
+        index="shift", columns=["type", "estimator"], values="acc_std"
+    )
+    df_tab = df_tab.reindex(
+        columns=["NO DA", "Reweighting", "Mapping", "Subspace", "Other"], level=0
+    )
+    df_tab_acc_std = df_tab_acc_std.reindex(
+        columns=["NO DA", "Reweighting", "Mapping", "Subspace", "Other"], level=0
+    )
+
+    df_tab = df_tab.reindex(
+        columns=[
+            "Train Src",
+            "Train Tgt",
+            "Dens. RW",
+            "Disc. RW",
+            "Gauss. RW",
+            "KLIEP",
+            "KMM",
+            "NN RW",
+            "MMDTarS",
+            "CORAL",
+            "MapOT",
+            "EntOT",
+            "ClassRegOT",
+            "LinOT",
+            "MMD-LS",
+            "JPCA",
+            "SA",
+            "TCA",
+            "TSL",
+            "JDOT",
+            "OTLabelProp",
+            "DASVM",
+        ],
+        level=1,
+    )
+    df_tab_acc_std = df_tab_acc_std.reindex(
+        columns=[
+            "Train Src",
+            "Train Tgt",
+            "Dens. RW",
+            "Disc. RW",
+            "Gauss. RW",
+            "KLIEP",
+            "KMM",
+            "NN RW",
+            "MMDTarS",
+            "CORAL",
+            "MapOT",
+            "EntOT",
+            "ClassRegOT",
+            "LinOT",
+            "MMD-LS",
+            "JPCA",
+            "SA",
+            "TCA",
+            "TSL",
+            "JDOT",
+            "OTLabelProp",
+            "DASVM",
+        ],
+        level=1,
+    )
+    df_tab = df_tab.T.rename(
+        index={
+            "NO DA": r"\rotatebox[origin=c]{90}{}",
+            "Reweighting": r"\rotatebox[origin=c]{90}{Reweighting}",
+            "Mapping": r"\rotatebox[origin=c]{90}{Mapping}",
+            "Subspace": r"\rotatebox[origin=c]{90}{Subspace}",
+            "Other": r"\rotatebox[origin=c]{90}{Other}",
+        }
+    )
+
+    df_tab_acc_std = df_tab_acc_std.T.rename(
+        index={
+            "NO DA": r"\rotatebox[origin=c]{90}{}",
+            "Reweighting": r"\rotatebox[origin=c]{90}{Reweighting}",
+            "Mapping": r"\rotatebox[origin=c]{90}{Mapping}",
+            "Subspace": r"\rotatebox[origin=c]{90}{Subspace}",
+            "Other": r"\rotatebox[origin=c]{90}{Other}",
+        }
+    )
+
+    if df_dataset["dataset"].values[0] == "Simulated":
+        df_tab = df_tab.reindex(
+            columns=[
+                "covariate_shift",
+                "target_shift",
+                "concept_drift",
+                "subspace",
+            ],
+        )
+        df_tab = df_tab.rename(
+            columns={
+                "covariate_shift": "\\underline{Cov. shift}",
+                "target_shift": "\\underline{Tar. shift}",
+                "concept_drift": "\\underline{Cond. shift}",
+                "subspace": "\\underline{Sub. shift}",
+            }
+        )
+        df_tab_acc_std = df_tab_acc_std.reindex(
+            columns=[
+                "covariate_shift",
+                "target_shift",
+                "concept_drift",
+                "subspace",
+            ],
+        )
+        df_tab_acc_std = df_tab_acc_std.rename(
+            columns={
+                "covariate_shift": "\\underline{Cov. shift}",
+                "target_shift": "\\underline{Tar. shift}",
+                "concept_drift": "\\underline{Cond. shift}",
+                "subspace": "\\underline{Sub. shift}",
+            }
+        )
+    df_mean_dataset = (
+        df_dataset.groupby(["estimator", "scorer"])[
+            "target_accuracy-test-mean", "target_accuracy-test-std"
+        ]
+        .mean()
+        .reset_index()
+    )
+    df_mean_dataset["Mean"] = df_mean_dataset["target_accuracy-test-mean"]
+    # rename accuracy to mean
+    df_tab = df_tab.reset_index().merge(
+        df_mean_dataset[["estimator", "Mean"]], on="estimator"
+    )
+    df_mean_dataset["Mean"] = (
+        df_mean_dataset["target_accuracy-test-mean"].round(2).astype(str)
+        + " $\pm$ "
+        + df_mean_dataset["target_accuracy-test-std"].round(2).astype(str)
+    )
+    df_tab_acc_std = df_tab_acc_std.reset_index().merge(
+        df_mean_dataset[["estimator", "Mean"]],
+        on="estimator",
+    )
+    df_tab = df_tab.merge(df_rank, on="estimator")
+
+    df_tab = df_tab.set_index(["type", "estimator"])
+    df_tab = df_tab.round(2)
+
+    df_tab_acc_std = df_tab_acc_std.set_index(["type", "estimator"])
+
+    for i, col in enumerate(df_tab.columns[:-1]):
+        max_value = df_tab.loc[df_tab[col].index[1], col]
+        min_value = df_tab.loc[df_tab[col].index[0], col]
+        for idx in df_tab.index[2:]:
+            # get the value
+            if df_tab.loc[idx, col] == "nan" or np.isnan(df_tab.loc[idx, col]):
+                continue
+            value = df_tab.loc[idx, col]
+            final_value = df_tab_acc_std.loc[idx, col]
+            # get the color
+            color = shade_of_color(
+                value,
+                final_value,
+                min_value=min_value,
+                max_value=max_value,
+            )
+            df_tab.loc[idx, col] = color
+        df_tab.loc[df_tab.index[1], col] = "\\cellcolor{green_color!%d}{%s}" % (
+            70,
+            df_tab_acc_std.loc[df_tab.index[1], col],
+        )
+        df_tab.loc[df_tab.index[0], col] = df_tab_acc_std.loc[df_tab.index[0], col]
+
+        # apply mcrot on the columns
+        df_tab = df_tab.rename(
+            columns={
+                col: "\mcrot{1}{l}{45}{" + str(col) + "}",
+            }
+        )
+    df_tab = df_tab.rename(
+        columns={
+            "rank": "\mcrot{1}{l}{45}{Rank}",
+        }
+    )
+
+    df_tab = df_tab.fillna("\\color{gray!90}NA")
+
+    # convert to latex
+    lat_tab = df_tab.to_latex(
+        escape=False,
+        multicolumn_format="c",
+        multirow=True,
+        # remove [t] in multirow
+        column_format="|l|l||" + "r|" * (len(df_tab.columns) - 1) + "|r|",
+        # round value
+        # float_format="%.2f",
+        # put the name of multirow in vertical
+    )
+    lat_tab = lat_tab.replace("toprule", "hline")
+    lat_tab = lat_tab.replace("midrule", "hline")
+    lat_tab = lat_tab.replace(
+        "cline{1-" + f"{2 + len(df_tab.columns)}" + "}", "hline\hline"
+    )
+    lat_tab = lat_tab.replace("\multirow[t]", "\multirow")
+    lat_tab = lat_tab.replace("bottomrule", "hline")
+
+    # save in txt file
+    with open(f"table_results_{df['dataset'].values[0]}.txt", "w") as f:
+        f.write(lat_tab)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate table results per dataset")
+
+    parser.add_argument("--dataset", type=str, help="dataset name", default="simulated")
+
+    parser.add_argument(
+        "--csv-file",
+        type=str,
+        help="Path to the csv file containing results for real data",
+        default="./readable_csv/results_all_datasets_experiments.csv",
+    )
+
+    parser.add_argument(
+        "--csv-file-simulated",
+        type=str,
+        help="Path to the csv file containing results for simulated data",
+        default="./readable_csv/simulated_31_05_readable.csv",
+    )
+
+    args = parser.parse_args()
+
+    df = generate_table_results(
+        args.dataset, args.csv_file, args.csv_file_simulated
+    )
---
+++ visualize/plot_supervised_vs_unsupervised.py
@@ -0,0 +1,187 @@
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import argparse
+
+
+def generate_scatter(csv_file):
+    df = pd.read_csv(csv_file)
+
+    df_target = df.query('estimator == "Train Tgt" & scorer == "supervised"')
+    df_source = df.query('estimator == "Train Src" & scorer == "best_scorer"')
+    df = df.merge(
+        df_target[["shift", "target_accuracy-test-mean", "target_accuracy-test-std"]],
+        on="shift",
+        suffixes=("", "_target"),
+    )
+    df = df.merge(
+        df_source[["shift", "target_accuracy-test-mean", "target_accuracy-test-std"]],
+        on="shift",
+        suffixes=("", "_source"),
+    )
+    df["accn"] = (df["target_accuracy-test-mean"] - df["target_accuracy-test-mean_source"]) / (
+        df["target_accuracy-test-mean_target"] - df["target_accuracy-test-mean_source"]
+    )
+
+    df["stdn"] = df["target_accuracy-test-std"] / np.abs(
+        (df["target_accuracy-test-mean_target"] - df["target_accuracy-test-mean_source"])
+    )
+    # remove rows where the source is better than the target
+    df = df[df["target_accuracy-test-mean_source"] < df["target_accuracy-test-mean_target"]].reset_index()
+
+    # filtering
+    df = df.query("estimator != 'NO_DA_SOURCE_ONLY_BASE_ESTIM'")
+
+    df_mean = (
+        df.groupby(["dataset", "type", "scorer", "estimator"])["target_accuracy-test-mean",]
+        .mean()
+        .reset_index()
+    )
+    # remove estimators
+    df_mean = df_mean.query("estimator != 'Train Tgt'")
+    df_mean = df_mean.query("estimator != 'Train Src'")
+
+    df_supervised = df_mean.query('scorer == "supervised"')
+    df_tot = df_mean.merge(
+        df_supervised[["estimator", "dataset", "target_accuracy-test-mean",]],
+        on=["estimator", "dataset"],
+        suffixes=("", "_supervised"),
+    )
+    df_tot = df_tot.query("scorer != 'supervised' & scorer != 'best_scorer'")
+
+    fig, axes = plt.subplots(1, 5, figsize=(11, 1.9), sharey=True)
+    name_scorer = ["IW", "SND", "DEV", "PE", "CircV"]
+    scorers = [
+        "importance_weighted",
+        "soft_neighborhood_density",
+        "deep_embedded_validation",
+        "prediction_entropy",
+        "circular_validation",
+    ]
+
+    for i, scorer in enumerate(scorers):
+        df_scorer = df_tot.query(f'scorer == "{scorer}"')
+        df_scorer.rename(columns={"type": "Method type", "dataset": "Dataset"}, inplace=True)
+        if i < 4:
+            sns.scatterplot(
+                data=df_scorer,
+                x="target_accuracy-test-mean_supervised",
+                y="target_accuracy-test-mean",
+                hue="Method type",
+                ax=axes[i],
+                legend=False,
+                alpha=0.6,
+                s=50,
+                edgecolor="gray",
+                palette="colorblind",
+            )
+        else:
+            sns.scatterplot(
+                data=df_scorer,
+                x="target_accuracy-test-mean_supervised",
+                y="target_accuracy-test-mean",
+                hue="Method type",
+                ax=axes[i],
+                alpha=0.6,
+                s=50,
+                # change line color
+                palette="colorblind",
+                edgecolor="gray",
+            )
+            # change legend position,
+            axes[i].legend(loc="upper left", bbox_to_anchor=(1.1, 1.), fontsize=8)
+        # get lims of the plot
+        lims = [
+            np.min([axes[i].get_xlim(), axes[i].get_ylim()]),
+            np.max([axes[i].get_xlim(), axes[i].get_ylim()]),
+        ]
+        # plot the diagonal
+        axes[i].plot(lims, lims, "k--", alpha=0.75, zorder=0)
+        axes[i].set_xlim(0, 1)
+        axes[i].set_ylim(0, 1)
+        axes[i].set_aspect("equal")
+        axes[i].set_title(f"{name_scorer[i]}")
+        axes[i].set_xlabel("Supervised scorer")
+        axes[i].set_ylabel("")
+    axes[0].set_ylabel("Unsupervised scorer")
+    fig.savefig("supervised_vs_unsupervised.pdf", bbox_inches="tight")
+
+    fig, axes = plt.subplots(4, 5, figsize=(12, 9), sharex=True, sharey=True)
+    name_scorer = ["IW", "SND", "DEV", "PE", "CircV"]
+    scorers = [
+        "importance_weighted",
+        "soft_neighborhood_density",
+        "deep_embedded_validation",
+        "prediction_entropy",
+        "circular_validation",
+    ]
+    types = [
+        "Reweighting",
+        "Mapping",
+        "Subspace",
+        "Other",
+    ]
+
+    for i, scorer in enumerate(scorers):
+        for j, type in enumerate(types):
+            df_scorer_type = df_tot.query(f'scorer == "{scorer}" & type == "{type}"')
+            if i < 4 or j < 3:
+                sns.scatterplot(
+                    data=df_scorer_type,
+                    x="target_accuracy-test-mean_supervised",
+                    y="target_accuracy-test-mean",
+                    # hue="type",
+                    hue="dataset",
+                    ax=axes[j, i],
+                    legend=False,
+                    alpha=0.7,
+                    palette="colorblind",
+                    s=80,
+                    edgecolor="gray",
+                )
+            else:
+                sns.scatterplot(
+                    data=df_scorer_type,
+                    x="target_accuracy-test-mean_supervised",
+                    y="target_accuracy-test-mean",
+                    # hue="type",
+                    hue="dataset",
+                    ax=axes[j, i],
+                    alpha=0.7,
+                    palette="colorblind",
+                    s=80,
+                    # change line color
+                    edgecolor="gray",
+                    legend=True,
+                )
+                axes[j, i].legend(fontsize=6)
+            if j == 0:
+                axes[j, i].set_title(f"{name_scorer[i]}")
+            if i == 0:
+                axes[j, i].set_ylabel(f"{type}\n Unsupervised Scorer")
+            # plot the diagonal
+            axes[j, i].plot([0, 1], [0, 1], "k--", alpha=0.75, zorder=0)
+            axes[j, i].set_xlim(0, 1)
+            axes[j, i].set_ylim(0, 1)
+            axes[j, i].set_aspect("equal")
+            axes[j, i].set_xlabel("Supervised Scorer")
+    fig.savefig("supervised_vs_unsupervised_big_table.pdf", bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Compute scatter plot"
+    )
+
+    parser.add_argument(
+        "--csv-file",
+        type=str,
+        help="Path to the csv file containing results for real data",
+        default='./readable_csv/results_all_datasets_experiments.csv'
+    )
+
+    args = parser.parse_args()
+
+    df = generate_scatter(args.csv_file)
