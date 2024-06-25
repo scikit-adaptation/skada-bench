d582974d8779507f84c0bfb82705010bce1f6a44
Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>
2024-06-25 09:10:21 +0200

    Add a scorers file where users can easily add a scorer (#136)

---
+++ benchmark_utils/scorers.py
@@ -0,0 +1,37 @@
+"""
+This script imports and initializes various scoring metrics used for evaluating domain adaptation techniques.
+It utilizes `benchopt`'s `safe_import_context` to ensure smooth importing and handling of dependencies.
+
+Adding a New Scorer
+-------------------
+
+To add a new scorer to the script:
+
+1. **Import the Scorer**: Add the import statement for the new scorer class within the `safe_import_context` block.
+2. **Update `CRITERIONS` Dictionary**: Add a new entry in the `CRITERIONS` dictionary with the scorer's name and its initialized instance.
+
+This method ensures that all scorers are organized and easily accessible for evaluating domain adaptation methods.
+"""
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from skada.metrics import (
+        SupervisedScorer,
+        PredictionEntropyScorer,
+        ImportanceWeightedScorer,
+        SoftNeighborhoodDensity,
+        DeepEmbeddedValidation,
+        CircularValidation,
+    )
+
+CRITERIONS = {
+    'supervised': SupervisedScorer(),
+    'prediction_entropy': PredictionEntropyScorer(),
+    'importance_weighted': ImportanceWeightedScorer(),
+    'soft_neighborhood_density': SoftNeighborhoodDensity(),
+    'deep_embedded_validation': DeepEmbeddedValidation(),
+    'circular_validation': CircularValidation()
+}
