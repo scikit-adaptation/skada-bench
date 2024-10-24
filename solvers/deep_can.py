97012ca62625d80e403b8f60974b0a8a15fabe71
Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>
2024-10-24 16:11:56 +0200

    remove unused import

--- solvers/deep_can.py
+++ solvers/deep_can.py
@@ -6,7 +6,6 @@ from benchopt import safe_import_context
 with safe_import_context() as import_ctx:
     from benchmark_utils.deep_base_solver import DeepDASolver
     from benchmark_utils.utils import get_params_per_dataset
-    from benchmark_utils.backbones_architecture import DomainClassifier
     from skada.deep import CAN
     from skada.metrics import (
         SupervisedScorer, DeepEmbeddedValidation,
