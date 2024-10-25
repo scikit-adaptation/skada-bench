b686fb972e46eb56c66f058b4afde20d81cbb4f8
Théo Gnassounou <66993815+tgnassou@users.noreply.github.com>
2024-10-25 14:08:11 +0200

    [MRG] Add DAN (#191)
    * add dan
    
    * add dan
    
    * add dan
    
    * Rename dan --> deep_dan  + fix benchopt test
    
    * Fix benchopt test
    
    * Update solvers/deep_dan.py
    
    ---------
    
    Co-authored-by: Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

---
+++ solvers/deep_dan.py
@@ -0,0 +1,45 @@
+from benchopt import safe_import_context
+
+# Protect the import with `safe_import_context()`. This allows:
+# - skipping import to speed up autocompletion in CLI.
+# - getting requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    from benchmark_utils.deep_base_solver import DeepDASolver
+    from benchmark_utils.utils import get_params_per_dataset
+    from skada.deep import DAN
+
+    from benchmark_utils.deep_base_solver import import_ctx as base_import_ctx
+    if base_import_ctx.failed_import:
+        exc, val, tb = base_import_ctx.import_error
+        raise exc(val).with_traceback(tb)
+
+
+# The benchmark solvers must be named `Solver` and
+# inherit from `BaseSolver` for `benchopt` to work properly.
+class Solver(DeepDASolver):
+    # Name to select the solver in the CLI and to display the results.
+    name = 'deep_dan'
+
+    # List of parameters for the solver. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    default_param_grid = {
+        'criterion__reg': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3],
+    }
+
+    def get_estimator(self, n_classes, device, dataset_name, **kwargs):
+        dataset_name = dataset_name.split("[")[0].lower()
+
+        params = get_params_per_dataset(
+            dataset_name, n_classes,
+        )
+
+        net = DAN(
+            **params,
+            layer_name="feature_layer",
+            train_split=None,
+            device=device,
+            warm_start=True,
+        )
+
+        return net
