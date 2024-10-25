6ff297b4b58675514ea882f20c0d22a646a336ef
Thomas Moreau <thomas.moreau.2010@gmail.com>
2024-10-25 13:51:40 +0200

    Update solvers/deep_dan.py

--- solvers/deep_dan.py
+++ solvers/deep_dan.py
@@ -4,7 +4,7 @@ from benchopt import safe_import_context
 # - skipping import to speed up autocompletion in CLI.
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
-    from benchmark_utils.base_solver import DeepDASolver
+    from benchmark_utils.deep_base_solver import DeepDASolver
     from benchmark_utils.utils import get_params_per_dataset
     from skada.deep import DAN
 
