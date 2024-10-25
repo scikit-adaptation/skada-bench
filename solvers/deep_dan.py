c2026ceca82ddb7e98d31ac20f9cecc7770198c9
Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>
2024-10-25 10:46:42 +0200

    Rename dan --> deep_dan  + fix benchopt test

--- solvers/deep_dan.py
+++ solvers/deep_dan.py
@@ -8,12 +8,17 @@ with safe_import_context() as import_ctx:
     from benchmark_utils.base_solver import DeepDASolver
     from skada.deep import DAN
 
+    from benchmark_utils.deep_base_solver import import_ctx as base_import_ctx
+    if base_import_ctx.failed_import:
+        exc, val, tb = base_import_ctx.import_error
+        raise exc(val).with_traceback(tb)
+
 
 # The benchmark solvers must be named `Solver` and
 # inherit from `BaseSolver` for `benchopt` to work properly.
 class Solver(DeepDASolver):
     # Name to select the solver in the CLI and to display the results.
-    name = 'dan'
+    name = 'deep_dan'
 
     # List of parameters for the solver. The benchmark will consider
     # the cross product for each key in the dictionary.
@@ -23,7 +28,6 @@ class Solver(DeepDASolver):
     }
 
     def get_estimator(self, n_classes, device, dataset_name, **kwargs):
-
         dataset_name = dataset_name.split("[")[0].lower()
 
         params = get_params_per_dataset(
