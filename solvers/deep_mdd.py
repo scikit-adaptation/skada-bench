55a4b595755c072148b028c48d2db3fa35a7905a
Yanis Lalou <57602694+YanisLalou@users.noreply.github.com>
2024-11-05 15:16:56 +0100

    [MRG] Add MDD method (#195)
    * Add MDD method
    
    * Add warm_start to dann + add domain_classifier arg to mdd
    
    * fix arg name deep_mdd
    
    * Fix DomainClassifier args
    
    * Change import path of DomainClassifier

--- solvers/deep_mdd.py
+++ solvers/deep_mdd.py
@@ -6,8 +6,8 @@ from benchopt import safe_import_context
 with safe_import_context() as import_ctx:
     from benchmark_utils.deep_base_solver import DeepDASolver
     from benchmark_utils.utils import get_params_per_dataset
-    from benchmark_utils.backbones_architecture import DomainClassifier
-    from skada.deep import DANN
+    from skada.deep.modules import DomainClassifier
+    from skada.deep import MDD
 
     from benchmark_utils.deep_base_solver import import_ctx as base_import_ctx
     if base_import_ctx.failed_import:
@@ -19,42 +19,32 @@ with safe_import_context() as import_ctx:
 # inherit from `BaseSolver` for `benchopt` to work properly.
 class Solver(DeepDASolver):
     # Name to select the solver in the CLI and to display the results.
-    name = 'deep_dann'
+    name = 'deep_mdd'
 
     # List of parameters for the solver. The benchmark will consider
     # the cross product for each key in the dictionary.
     # All parameters 'p' defined here are available as 'self.p'.
     default_param_grid = {
-        # 'criterion__reg': np.logspace(-3, 0, 4),
-        'criterion__reg': [1e-3, 1e-2, 1e-1, 1],
+        'criterion__reg': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3],
     }
 
     def get_estimator(self, n_classes, device, dataset_name, **kwargs):
-
         dataset_name = dataset_name.split("[")[0].lower()
 
         params = get_params_per_dataset(
             dataset_name, n_classes,
         )
-        # Reduce learning rate and increase momentum
-        params['lr'] = params['lr'] * 0.1
-        if 'optimizer__momentum' in params:
-            params['optimizer__momentum'] = 0.9
 
-        net = DANN(
+        net = MDD(
             **params,
             layer_name="feature_layer",
             train_split=None,
             device=device,
-            domain_classifier=DomainClassifier(
-                num_features=params['module'].n_features
-            ),
             warm_start=True,
-            # optimizer__param_groups=[
-            #     ('base_module_.feature_layer*', {'lr': params['lr']}),
-            #     ('base_module_.final_layer*', {'lr': params['lr']}),
-            #     ('domain_classifier_*', {'lr': params['lr'],}),
-            # ]
+            disc_classifier=DomainClassifier(
+                num_features=params['module'].n_features,
+                n_classes=n_classes,
+            ),
         )
 
         return net
